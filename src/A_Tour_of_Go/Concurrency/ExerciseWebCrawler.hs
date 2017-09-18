5f874dacfa4a176006133b69cc319177a331808a
syocy <osmium.k@gmail.com>
2017-09-19 07:35:53 +0900

    tmp

--- src/A_Tour_of_Go/Concurrency/ExerciseWebCrawler.hs
+++ src/A_Tour_of_Go/Concurrency/ExerciseWebCrawler.hs
@@ -6,7 +6,9 @@ import Control.Monad (forM_, forM, when)
 import Data.Set (Set)
 import qualified Data.Set as Set
 import Control.Concurrent.Async (async, wait, withAsync, forConcurrently_)
-import Control.Concurrent.STM (atomically, TVar, newTVar, modifyTVar', readTVar)
+import Control.Concurrent.STM ( atomically
+                              , TVar, newTVar, modifyTVar', readTVar
+                              , TQueue, newTQueue, writeTQueue, readTQueue)
 import Control.DeepSeq (deepseq)
 
 data FetchResult = Fetched { fetchedBody :: String, fetchedUrls :: [String] }
@@ -30,27 +32,28 @@ crawlNaive url depth f | depth <= 0 = return ()
 crawl :: (Fetcher f) => String -> Int -> f -> IO ()
 crawl url depth f = do
   cache <- atomically $ newTVar Set.empty
-  crawl' url depth f cache
+  retQueue <- atomically $ newTQueue
+  crawl' url depth f cache retQueue
+  --
 
-crawl' :: (Fetcher f) => String -> Int -> f -> TVar (Set String) -> IO ()
-crawl' url depth f cache | depth <= 0 = return ()
-                         | otherwise  = do
+crawl' :: (Fetcher f) => String -> Int -> f -> TVar (Set String) -> TQueue String -> IO ()
+crawl' url depth f cache retQueue | depth <= 0 = return ()
+                                  | otherwise  = do
   reserved <- tryReserveUrl url cache
   when reserved $ do
     fetchResult <- fetch f url
     case fetchResult of
-      FetchError mes -> putStrLn mes
+      FetchError mes -> atomically $ writeTQueue retQueue mes
       Fetched body subUrls -> do
-        putStrLn $ "found: " ++ url ++ " \"" ++ body ++ "\""
-        -- forConcurrently_ subUrls $ \subUrl -> do
+        atomically $ writeTQueue retQueue $ "found: " ++ url ++ " \"" ++ body ++ "\""
+        forConcurrently_ subUrls $ \subUrl -> do
+          crawl' subUrl (depth - 1) f cache retQueue
+        -- as <- forM subUrls $ \subUrl -> do
         --   print subUrl
-        --   crawl' subUrl (depth - 1) f cache
-        as <- forM subUrls $ \subUrl -> do
-          print subUrl
-          async $ do
-            print subUrl
-            crawl' subUrl (depth - 1) f cache
-        forM_ as wait
+        --   async $ do
+        --     print subUrl
+        --     crawl' subUrl (depth - 1) f cache
+        -- forM_ as wait
 
 tryReserveUrl :: String -> TVar (Set String) -> IO Bool
 tryReserveUrl url cache = atomically $ do
@@ -65,7 +68,7 @@ tryReserveUrl url cache = atomically $ do
 main :: IO ()
 main = do
   -- crawlNaive "http://golang.org/" 4 fetcher
-  crawl "http://golang.org/" 2 fetcher
+  crawl "http://golang.org/" 4 fetcher
 
 data FakeResult = FakeResult
                   { fakeBody :: String
