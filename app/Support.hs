90b627b94430a27715001ca2c4d2b82094158bec
syocy <osmium.k@gmail.com>
2017-11-02 20:29:21 +0900

    fix parser

--- app/Support.hs
+++ app/Support.hs
@@ -4,23 +4,16 @@ import Text.Megaparsec
 import Text.Megaparsec.Text
 import qualified Data.Text as T
 
-dirLayer :: Parser String
-dirLayer = manyTill anyChar (char '/')
-
 localeParser :: Parser String
 localeParser = (many $ try dirLayer) >> manyTill anyChar yamlSuffix
   where
+    dirLayer :: Parser String
+    dirLayer = manyTill anyChar (char '/')
     yamlSuffix :: Parser String
     yamlSuffix = string ".yaml" <|> string ".yml"
 
 parseLocale :: FilePath -> Either (ParseError Char Dec) String
 parseLocale filename = parse localeParser "" $ T.pack filename
-
--- filenameParser :: Parser String
--- filenameParser = (many $ try dirLayer) >> many anyChar
--- 
--- parseFilename :: FilePath -> Either (ParseError Char Dec) String
--- parseFilename filename = parse filenameParser "" $ T.pack filename
 
 postParser :: String -> Parser String
 postParser prefix = manyTill anyChar (string prefix) >> many anyChar
