99971504e79816dae44febcdcdbccb9e5d8555bc
syocy <osmium.k@gmail.com>
2017-11-08 21:02:51 +0900

    tmp

--- app/Main.hs
+++ app/Main.hs
@@ -8,22 +8,24 @@ import Support
 import Text.Heterocephalus
 import Text.Blaze.Renderer.Text (renderMarkup)
 import qualified Data.Text.Lazy.IO as LT
-import System.Directory (createDirectoryIfMissing)
 import qualified Data.Yaml as Yaml
 import Data.Either (either)
-import Prelude hiding ((!!))
+import Prelude hiding ((!!), FilePath)
 import Data.Monoid ((<>))
 import System.FilePath.Glob (glob)
 import Control.Monad (forM_)
 import Language.Haskell.TH
+import Turtle
+import Data.String (fromString)
 
 main :: IO ()
 main = do
   generateHtmlFiles
+  copyFiles
 
 generateHtmlFiles :: IO ()
 generateHtmlFiles = do
-  createDirectoryIfMissing True "target"
+  mktree "target"
   iFilenames <- glob "static/i18n/*.yaml"
   forM_ iFilenames $ \iFilename -> do
     iMaybe <- Yaml.decodeFile iFilename :: IO (Maybe Yaml.Value)
@@ -34,16 +36,23 @@ generateHtmlFiles = do
     case localeEither of
       Left e -> print e
       Right locale -> do
-        let dirname = "target/" <> locale <> "/"
-        createDirectoryIfMissing True dirname
+        let localeDir = "target/" <> locale <> "/"
+        mktree $ fromString $ localeDir <> "concurrency/"
         let compiledDict =
               $(do
-                   hFilenames <- runIO $ glob "static/heterocephalus/*.html"
+                   hFilenames <- runIO $ glob "static/heterocephalus/**/*.html"
                    let names = map (either (error . show) id . parsePost "static/heterocephalus/") hFilenames
                    namesE <- mapM (\x -> [|x|]) names
                    compiledFilesE <- mapM compileHtmlFile hFilenames
                    return $ ListE $ zipWith (\x y -> TupE [x,y]) namesE compiledFilesE
                )
         forM_ compiledDict $ \(name, compiled) -> do
-          LT.writeFile (dirname <> name) $ renderMarkup compiled
+          LT.writeFile (localeDir <> name) $ renderMarkup compiled
 
+copyFiles :: IO ()
+copyFiles = do
+  mktree "target"
+  mktree "target/css"
+  cptree "static/css" "target/css"
+  mktree "target/images"
+  cptree "static/images" "target/images"
