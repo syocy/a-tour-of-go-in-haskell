9374a185b0f0b3e1d681ad4070dfd52a7f3f3819
syocy <osmium.k@gmail.com>
2017-11-13 03:26:57 +0900

    mix lucid

--- app/Main.hs
+++ app/Main.hs
@@ -1,12 +1,17 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE TypeFamilies #-}
 
 module Main where
 
 import Support
 import Text.Heterocephalus
 import Text.Blaze.Renderer.Text (renderMarkup)
+import qualified Data.Text as T
 import qualified Data.Text.Lazy as LT
 import qualified Data.Text.Lazy.IO as LT
 import qualified Data.Yaml as Yaml
@@ -18,61 +23,93 @@ import Control.Monad (forM, forM_)
 import Language.Haskell.TH
 import Turtle
 import Data.String (fromString)
+import Data.Extensible
+import Control.Lens hiding ((:>))
+import Lucid
 
-do
-  sources <- runIO $ do
-    cFilenames <- glob "src/A_Tour_of_Go/Concurrency/*.hs"
-    let escape x = case x of
-          '"' -> "\\\""
-          '\n' -> "\\n"
-          a -> LT.singleton a
-    let isNotComment = not . isComment . LT.toStrict
-    let removeComment = LT.unlines . reverse . tail . reverse . filter isNotComment . LT.lines
-    contents <- map (LT.concatMap escape . removeComment) <$> mapM LT.readFile cFilenames
-    let names = map (either (error . show) id . parseFilename) cFilenames
-    return $ zip names contents
-  ret <- forM sources $ \(name, content) -> do
-    let name' = mkName $ "code" <> name
-    let contentStr = LT.unpack content
-    let v = ValD (VarP name') (NormalB (LitE (StringL contentStr))) []
-    strTMaybe <- lookupTypeName "String"
-    let strT = case strTMaybe of
-          Just a -> a
-          Nothing -> error ""
-    let ann = SigD name' (ConT strT)
-    return [ann, v]
-  return $ concat ret
+mkField "localeF dictF"
+type I18N = Record
+  [ "localeF" :> T.Text
+  , "dictF" :> Yaml.Value
+  ]
 
-main :: IO ()
-main = do
-  generateHtmlFiles
-  copyFiles
+loadI18NFiles :: IO [I18N]
+loadI18NFiles = do
+  paths <- glob "static/i18n/*.yaml"
+  forM paths $ \path -> do
+    let locale = case parseLocale path of
+          Left e -> error $ show e
+          Right x -> T.pack x
+    iMaybe <- Yaml.decodeFile path :: IO (Maybe Yaml.Value)
+    let i = case iMaybe of
+          Nothing -> error $ "failed to decode: " <> path
+          Just x -> x
+    return $
+      localeF @= locale
+      <: dictF @= i
+      <: nil
+
+mkField "titleF srcF targetF genArticleF"
+type Page = Record
+  [ "titleF" :> T.Text
+  , "srcF" :> T.Text
+  , "targetF" :> T.Text
+  , "genArticleF" :> (I18N -> Html ())
+  ]
+
+toPage :: (T.Text, T.Text, T.Text, I18N -> Html ()) -> Page
+toPage (name, src, target, genArticle) =
+  titleF @= name
+  <: srcF @= src
+  <: targetF @= target
+  <: genArticleF @= genArticle
+  <: nil
+
+pages :: [Page]
+pages = map toPage
+  [ ( "Goroutines", "A_Tour_of_Go/Concurrency/Goroutines.hs", "concurrency/goroutines.html", \i -> do
+        let at = at1 i "goroutines"
+        p_ $ at "first"
+        pre_ $ code_ "async $ f x y z"
+        p_ $ at "inter"
+        pre_ $ code_ "f x y z"
+        p_ ( at "overviewPreAsync"
+             <> a_ [href_ "https://hackage.haskell.org/package/async"] "async"
+             <> at "overviewPostAsync"
+           )
+        p_ ( at "detailPreBase"
+             <> a_ [href_ "https://hackage.haskell.org/package/base"] "base"
+             <> at "detailPostBase"
+           )
+    )
+  ]
+  where
+    at1 i key subKey = toHtmlRaw $ (i ^. dictF) !!! [key, subKey]
+
+clenseCode :: LT.Text -> LT.Text
+clenseCode = LT.concatMap escape . removeComment
+  where
+    escape x = case x of
+      '"' -> "\\\""
+      '\n' -> "\\n"
+      c -> LT.singleton c
+    isNotComment = not . isComment . LT.toStrict
+    removeComment = LT.unlines . reverse . tail . reverse . filter isNotComment . LT.lines
 
 generateHtmlFiles :: IO ()
 generateHtmlFiles = do
-  mktree "target"
-  iFilenames <- glob "static/i18n/*.yaml"
-  forM_ iFilenames $ \iFilename -> do
-    iMaybe <- Yaml.decodeFile iFilename :: IO (Maybe Yaml.Value)
-    let i = case iMaybe of
-          Nothing -> error $ "failed to decode: " <> iFilename
-          Just x -> x
-    let localeEither = parseLocale iFilename
-    case localeEither of
-      Left e -> print e
-      Right locale -> do
-        let localeDir = "target/" <> locale <> "/"
-        mktree $ fromString $ localeDir <> "concurrency/"
-        let compiledDict =
-              $(do
-                   hFilenames <- runIO $ glob "static/heterocephalus/**/*.html"
-                   let names = map (either (error . show) id . parsePost "static/heterocephalus/") hFilenames
-                   namesE <- mapM (\x -> [|x|]) names
-                   compiledFilesE <- mapM compileTextFile hFilenames
-                   return $ ListE $ zipWith (\x y -> TupE [x,y]) namesE compiledFilesE
-               )
-        forM_ compiledDict $ \(name, compiled) -> do
-          LT.writeFile (localeDir <> name) $ renderMarkup compiled
+  i18ns <- loadI18NFiles
+  forM_ pages $ \page -> do
+    rawCode <- LT.readFile $ T.unpack $ "src/" <> (page ^. srcF)
+    let code = clenseCode rawCode
+    forM_ i18ns $ \i18n -> do
+      let title = page ^. titleF
+      let article = renderText $ (page ^. genArticleF) i18n
+      let compiled = $(compileTextFile "static/heterocephalus/template.html")
+      let targetPath = "target/" <> (i18n^.localeF) <> "/" <> (page^.targetF)
+      let targetDirPath = fst $ T.breakOnEnd "/" targetPath
+      mktree $ fromString $ T.unpack targetDirPath
+      LT.writeFile (T.unpack targetPath) $ renderMarkup compiled
 
 copyFiles :: IO ()
 copyFiles = do
@@ -81,3 +118,7 @@ copyFiles = do
   cptree "static/css" "target/css"
   mktree "target/images"
   cptree "static/images" "target/images"
+
+main = do
+  generateHtmlFiles
+  copyFiles
