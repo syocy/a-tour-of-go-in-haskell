fd6039c1b12bf24644a4b4e7293f806129a34090
syocy <osmium.k@gmail.com>
2017-11-20 02:26:50 +0900

    simplify source

--- src/A_Tour_of_Go/Concurrency/BufferedChannels.hs
+++ src/A_Tour_of_Go/Concurrency/BufferedChannels.hs
@@ -7,7 +7,6 @@ import Control.Concurrent.STM (atomically, TBQueue, newTBQueue, readTBQueue, wri
 -- >>> main
 -- 1
 -- 2
--- 3
 main :: IO ()
 main = do
   ch <- newBoundedChan 2 :: IO (BoundedChan Int)
@@ -15,20 +14,15 @@ main = do
   writeChan ch 2
   print =<< readChan ch
   print =<< readChan ch
-  writeChan ch 3
-  print =<< readChan ch
 
 -- |
 -- >>> mainBySTM
 -- 1
 -- 2
--- 3
 mainBySTM :: IO ()
 mainBySTM = do
   ch <- atomically $ newTBQueue 2 :: IO (TBQueue Int)
   atomically $ writeTBQueue ch 1
   atomically $ writeTBQueue ch 2
   print =<< atomically (readTBQueue ch)
-  print =<< atomically (readTBQueue ch)
-  atomically $ writeTBQueue ch 3
   print =<< atomically (readTBQueue ch)
